a2655872c1ed4cd3ff117c51eb9d3946c54e1a23
alexiusbond <aleksey.bondarev@iaau.edu.kg>
2026-04-25 20:38:13 +0600

    new feature of lesson 7

--- bin/lesson_7/lesson_07.dart
+++ bin/lesson_7/lesson_07.dart
@@ -1,4 +1,37 @@
-void main() {
+import 'animals.dart';
+import 'interfaces.dart';
+import 'mixins.dart';
+import 'transports.dart';
 
-  // Start of program
+void main() {
+  Cat cat = Cat('Tom', 2);
+
+  List<Drawable> drawables = [
+    cat,
+    Car(),
+    Parrot('Polly', 5),
+    Plane(),
+    Snake('Kaa', 10),
+  ];
+
+  for (var element in drawables) {
+    drawAllVariants(element);
+    if (element is Animal) {
+      Animal a = element as Animal; // type casting
+      a.makeSound();
+      a.sleep();
+    }
+    if(element is Playable){
+      (element as Playable).play();
+    }
+    if(element is Fly){
+      (element as Fly).fly();
+      (element as Fly).flyHigh(element.runtimeType.toString());
+    }
+  }
+}
+
+void drawAllVariants(Drawable d) {
+  d.draw();
+  print(d.draw3D('plastic'));
 }
